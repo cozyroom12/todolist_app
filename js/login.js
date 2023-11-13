4f0c932bd627f0bdf0848e8bacf90235ae4793b7
cozyroom12 <wjddms6841@gmail.com>
2023-11-13 23:55:23 +0900

    fix login.js

--- js/login.js
+++ js/login.js
@@ -1,17 +1,18 @@
 const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
   const username = document.querySelector(".name").value;
   const password = document.querySelector(".password").value;
 
   const data = localStorage.getItem("users");
   const userList = JSON.parse(data); // array type
+
+  if (username == "Maru") {
+    console.log("the user is sensed. now what?");
+  }
 });
 
-const data = localStorage.getItem("users");
-const userList = JSON.parse(data);
-console.log(userList);
-
-const userNames = userList.forEach((user) => {
-  if (user.username == "input_value") console.log(user.username);
-});
+// const userNames = userList.forEach((user) => {
+//   if (user.username == "input_value") console.log(user.username);
+// });
