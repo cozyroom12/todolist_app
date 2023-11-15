6948c2169cd71b5bc3d0b0d2ef3482d503ad205c
cozyroom12 <wjddms6841@gmail.com>
2023-11-15 16:55:02 +0900

    add verification of login

--- js/login.js
+++ js/login.js
@@ -2,17 +2,29 @@ const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const username = document.querySelector(".name").value;
-  const password = document.querySelector(".password").value;
+  const usernameInput = document.querySelector(".name").value.toLowerCase();
+  const passwordInput = document.querySelector(".password").value;
 
   const data = localStorage.getItem("users");
   const userList = JSON.parse(data); // array type
 
-  if (username == "Maru") {
-    console.log("the user is sensed. now what?");
+  // if (usernameInput == "Maru") {
+  //   // how do i do upcap here??
+  //   console.log("the user is sensed. now what?");
+  // }
+
+  const user = userList.find(function (u) {
+    return u.username === usernameInput;
+  });
+
+  if (user) {
+    if (user.password === passwordInput) {
+      console.log("Login successful !");
+      window.location.href = "index.html";
+    } else {
+      console.log("Incorrect password");
+    }
+  } else {
+    console.log("Username not found");
   }
 });
-
-// const userNames = userList.forEach((user) => {
-//   if (user.username == "input_value") console.log(user.username);
-// });
